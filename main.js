e3ebfc0b9718013caa248ca1e5d5fa7542464a96
aqeel saad <sdqyl249@gmail.com>
2026-08-22 18:48:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
